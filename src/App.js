23ce13afe63c70ec229b62e9abbcfd947d902ddb
Suyash Maddhessiya <suyashmadhesia@gmail.com>
2022-06-27 13:09:59 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-
+import { useStateContext } from './contexts/ContextProvider';
 import "./App.css";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
@@ -12,7 +12,7 @@ import {
 } from './pages';
 
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
     return (
         <div>
             <BrowserRouter>
